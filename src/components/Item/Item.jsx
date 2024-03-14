18f52ac40d7b83db66713aecd80e1d6e9cd4b063
Marcos Giannasio <marcosgiannasio@gmail.com>
2024-03-14 15:28:35 -0300

    bootstrap

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,24 +2,21 @@ import { Link } from "react-router-dom";
 
 const Item = ({id, name, img, category, price}) => {
           return (
-                    <section className="container">
-                              
-                             <article className="row">
-                              <div className="card-group text-center p-5" style={{width: 500}}>
-                              <div className="card m-3">
-                              <img src ={img} className="card-img-top mx-auto rounded mt-3" style={{width: 200}} />
-                              <div className="card-body">
-                              <h1 className="card-title mt-3 mb-3">{name}</h1>
-                              <p className="card-text mt-3 mb-3 p-2">Categoría: {category}</p>
-                              <h1 className="h5">$ {price}</h1>
-                              </div>
-                              <div className="card-footer">
-                              <Link to = {`/detail/${id}`}className="btn btn-secondary mb-2">Ver Detalle</Link>
-                              </div>
-                              </div>
-                              </div>
-                              </article> 
-                    </section>
+
+<article class="row">
+  <div class="col-sm-3 text-center p-5">
+    <div class="card">
+      <div class="card-body">
+          <img  className="rounded" src={img} style={{width: 200}}/>
+        <h1 class="card-title mt-4 h2">{name}</h1>
+        <p>categoría: {category}</p>
+        <h2 class="h4">${price}</h2>
+        <p class="card-text"></p>
+        <Link to = {`/detail/${id}`}className="btn btn-secondary mb-2">Ver Detalle</Link>
+      </div>
+    </div>
+  </div>
+  </article>
           )
 }
 
